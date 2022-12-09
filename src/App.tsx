df6d7bf54315f4dec81b909e9f5dc016ab5f292c
starcoex <69073390+starcoex@users.noreply.github.com>
2022-12-09 11:48:51 +0900

    react typescript interface setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import styled from "styled-components";
+import Circle from "./Circle";
 
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const Title = styled.h1`
-  font-size: 45px;
-  color: ${(props) => props.theme.textColor};
-`;
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor='teal' />
+      <Circle bgColor='tomato' />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-const darkTheme = {
-  textColor: "whitesmoke",
-  bgColor: "#111",
-};
-const lightTheme = {
-  textColor: "#111",
-  bgColor: "whitesomke",
-};
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
